7a695ffec6d5b8b83f62bb77f85d0f6c9099fd08
Habibullah <103850736+habibullah46@users.noreply.github.com>
2022-05-16 07:08:53 -0700

    Create if-statments.cpp

---
+++ if-statments.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne=34; 
+	int valueTwo=54; 
+	int valueThree=34;
+	if( valueOne=valueTwo)
+	{
+		cout<<"Hello! My World...... \n";
+	}
+	else
+	{
+		cout<<"How Are u My World ......";
+		
+	}
+	if(valueTwo>valueThree)
+	{
+		cout<<" I Love Pakistan";
+		}
+	else
+		{
+			cout<<"Gilgit Baltistan";
+		}
+	
+}
